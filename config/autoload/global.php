5435738bb04a7ba5acf2e0f1b0566e850a65ac72
Sam Xiao <samxiaotj@gmail.com>
2014-08-22 14:38:32 +0800

    update config of DB

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -13,10 +13,10 @@
 return array(
     'db' => array(
         'driver' => 'Pdo',
-        'dsn' => 'mysql:dbname=wds;host=localhost',
-        'database' => 'wds',
-        'username' => 'root',
-        'password' => '',
+        'dsn' => 'mysql:dbname=test;host=localhost',
+        'database' => 'test',
+        'username' => 'admin',
+        'password' => 'admin',
         'hostname' => 'localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
