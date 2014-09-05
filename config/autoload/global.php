270f4b5e89c6f3169cce6bf95641d2736f04a46d
Sam Xiao <samxiaotj@gmail.com>
2014-09-05 14:53:12 +0800

    更新数据库设置

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,9 +14,9 @@ return array(
     'db' => array(
         'driver' => 'Pdo',
         'dsn' => 'mysql:dbname=test;host=localhost',
-        'database' => 'test',
-        'username' => 'admin',
-        'password' => 'admin',
+        'database' => 'wds',
+        'username' => 'root',
+        'password' => '',
         'hostname' => 'localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
