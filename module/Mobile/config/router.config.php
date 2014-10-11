ea0b36d50c066ff611b2281602da4b2bc67c80b1
Sam Xiao <samxiaotj@gmail.com>
2014-10-11 15:53:21 +0800

    update router

--- module/Mobile/config/router.config.php
+++ module/Mobile/config/router.config.php
@@ -1,24 +1,28 @@
 <?php
 return array(
-    'm.wds.local' => array(
+    'm.jd.rx-tech.cn' => array(
         'type' => 'Zend\Mvc\Router\Http\Hostname',
         'options' => array(
-            'route' => ':4th.[:3rd.]:2nd.:1st', // domain levels from right to left
+            'route' => ':5th.[:4th.]:3rd.:2nd.:1st', // domain levels from right to left
+
             'constraints' => array(
-                '4th' => 'm',
-                '3rd' => '.*?', // optional 3rd level domain such as .ci, .dev or .test
-                '2nd' => 'wds',
-                '1st' => 'local'
+                '5th' => 'm',
+                '4th' => '.*?', // optional level domain such as .ci, .dev or .test
+                '3rd' => 'jd',
+                '2nd' => 'rx-tech',
+                '1st' => 'cn'
             ),
 
             // Purposely omit default controller and action
             // to let the child routes control the route match
             'defaults' => array(
-                'module' => 'Admin',
-                'action' => 'index'
+                'module' => 'Mobile',
+                'action' => 'index',
+                '__NAMESPACE__' => 'Mobile\Controller'
             )
         ),
         'child_routes' => require (__DIR__ . "/child.router.config.php")
     ),
+
 
 );
